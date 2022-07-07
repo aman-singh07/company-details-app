7db638ac0568ce73b098bcff7d26904546b45e16
amansingh-bib <105208786+amansingh-bib@users.noreply.github.com>
2022-07-07 20:34:51 +0530

    added stock-details

--- app.js
+++ app.js
@@ -1,9 +1,27 @@
 var express = require("express");
+const axios = require('axios');
 const port = process.env.PORT || 3000
 var app = express();
-app.get("/url", (req, res, next) => {
-    res.json(["Tony","Lisa","Michael","Ginger","Food"]);
-   });
+
+function getStocksDetails(url){
+    return new Promise((resolve,reject) => {
+         try {
+             axios.get(url)
+             .then(response => {
+                 var codedata = response.data;
+                 resolve(codedata)
+             })
+         } catch (error) {
+             reject(error)
+         }
+    })
+ }
+
+
+app.get("/stock-details", async (req, res, next) => {
+    let stockData = await getStocksDetails("https://api.bseindia.com/BseIndiaAPI/api/ComHeader/w?quotetype=EQ&scripcode=500820&seriesid=");
+    res.send(stockData)
+});
 app.listen(port,() => {
   console.log(`Server running at port `+port);
 }); 
