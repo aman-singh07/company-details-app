331901d561aec7c6ed11186466757f1da6903df3
amansingh-bib <105208786+amansingh-bib@users.noreply.github.com>
2022-07-07 23:07:14 +0530

    added stocks-watch

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var express = require("express");
 const axios = require('axios');
 const port = process.env.PORT || 3000
 var app = express();
+app.use(express.urlencoded({limit: '50mb'}));
 
 function getStocksDetails(url){
     return new Promise((resolve,reject) => {
@@ -20,10 +21,10 @@ function getStocksDetails(url){
 
 app.get("/stock-details", async (req, res, next) => {
     //let result = [];
-    let url = 'https://api.bseindia.com/BseIndiaAPI/api/IndexMovers/w';//+req.params.id;
+    let url = 'https://www1.nseindia.com/live_market/dynaContent/live_watch/stock_watch/niftyStockWatch.json';//+req.params.id;
     let stockData = await getStocksDetails(url);
     //result.push();
-    res.send(stockData.Table)
+    res.send(stockData.data)
 });
 app.listen(port,() => {
   console.log(`Server running at port `+port);
